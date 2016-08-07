715a5565090578d05c3bba44ed9989feb193c6e5
Paulo <paulopereiradosanjos@gmail.com>
2016-08-07 20:12:52 -0300

    Exercicios 3 e 4 - Imprimindo numeros com loop while e loop for

---
+++ alura/java-I-primeiros-passos/Exercicio3.java
@@ -0,0 +1,9 @@
+class Exercicio3 {
+    public static void main(String[] args) {
+        int numero = 150;
+        while(numero < 300) {
+            System.out.println("nº: " + numero);
+            numero++;
+        }
+    }
+}
---
+++ alura/java-I-primeiros-passos/Exercicio4.java
@@ -0,0 +1,10 @@
+/*Imprima a soma de 1 até 1000 (inclusive).
+Para isso você deve utilizar alguma variável extra que armazena 
+a soma temporária e aumentá-la a cada novo passo. Compartilhe sua solução.*/
+class Exercicio4 {
+    public static void main(String[] args){
+        for(int i = 1; i <= 1000; i++){
+            System.out.println(i + " ");
+        }
+    }
+}
