9b3a1b3bbd62c204d1aacf80e4f56ff053977086
Paulo <paulopereiradosanjos@gmail.com>
2016-08-07 15:47:44 -0300

    Java 1 - Exercicios 1 e 2

---
+++ alura/java-I-primeiros-passos/Exercicio1.java
@@ -0,0 +1,12 @@
+class Exercicio1 {
+    public static void main(String[] args) {
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+        int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+        int mediaMensal = gastosTrimestre/3;
+
+        System.out.println("Gastos do trimestre " + gastosTrimestre);
+        System.out.println("Valor da média mensal = " + mediaMensal);
+    }
+}
