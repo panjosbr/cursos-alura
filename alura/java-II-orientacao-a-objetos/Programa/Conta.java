469778f7077488a3742c9eb6da5de7f5e43cae59
Paulo Pereira dos Anjos <panjos@J01.Jequiti.com.br>
2016-08-30 16:16:28 -0300

    Java 2.2 - Finalizando exercicios sobre heranca, override e polimorfismo

--- alura/java-II-orientacao-a-objetos/Programa/Conta.java
+++ alura/java-II-orientacao-a-objetos/Programa/Conta.java
@@ -33,3 +33,24 @@ class ContaPoupanca extends Conta {
         this.saldo += valor - 0.10;
     }
 }
+
+class AtualizadorDeContas {
+    private double saldoTotal = 0;
+    private double selic;
+
+    public AtualizadorDeContas(double selic) {
+        this.selic = selic;
+    }
+
+    public void roda(Conta c) {
+        System.out.println("===============================");
+        System.out.println("Saldo anterior: R$ " + c.getSaldo());
+        c.atualiza(this.selic);
+        System.out.println("Saldo atualizado: R$ " + c.getSaldo());
+        this.saldoTotal += c.getSaldo();
+    }
+
+    public double getSaldoTotal() {
+        return this.saldoTotal;
+    }
+}
---
+++ alura/java-II-orientacao-a-objetos/Programa/TesteContas.java
@@ -0,0 +1,21 @@
+class TesteContas {
+    public static void main(String[] args) {
+
+        Conta c = new Conta();
+        Conta cc = new ContaCorrente();
+        Conta cp = new ContaPoupanca();
+
+        c.deposita(1000);
+        cc.deposita(1000);
+        cp.deposita(1000);
+
+        AtualizadorDeContas adc = new AtualizadorDeContas(0.01);
+
+        adc.roda(c);
+        adc.roda(cc);
+        adc.roda(cp);
+
+        System.out.println("Saldo Total: R$ " + adc.getSaldoTotal());
+
+    }
+}
