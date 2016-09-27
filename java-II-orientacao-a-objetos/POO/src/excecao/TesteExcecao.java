9be12750e12234fd6ba31b2bd42c94aa835e470e
Paulo K3yboard <paulo.desenvolvedorjava@gmail.com>
2016-09-27 20:38:08 -0300

    Java 2.6.4 - outro teste com a excecao

--- java-II-orientacao-a-objetos/POO/src/excecao/TesteExcecao.java
+++ java-II-orientacao-a-objetos/POO/src/excecao/TesteExcecao.java
@@ -9,24 +9,21 @@ public class TesteExcecao {
 
 	static void metodo1() {
 		System.out.println("Inicio metodo 1");
-		metodo2();
+		try {
+			metodo2();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Error: " + e);
+		}
 		System.out.println("Fim metodo 1");
 	}
 
 	static void metodo2() {
 		System.out.println("Inicio metodo 2");
 		int[] array = new int[10];
-			for (int i = 0; i <= 15; i++) {
-				try {
-					array[i] = i;
-					System.out.println(i);
-				} catch (ArrayIndexOutOfBoundsException e) {
-					System.out.println("Erro " + e);
-				}
-			}
-
+		for (int i = 0; i <= 15; i++) {
+			array[i] = i;
+			System.out.println(i);
+		}
 		System.out.println("Fim metodo 2");
-
 	}
-
 }
