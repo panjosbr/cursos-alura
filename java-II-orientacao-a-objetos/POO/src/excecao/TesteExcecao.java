53afea98f6e786b01c4553b90579450e39d94ed6
Paulo K3yboard <paulo.desenvolvedorjava@gmail.com>
2016-09-26 22:42:13 -0300

    Java 2.6 - Iniciando exercicios sobre excecoes

---
+++ java-II-orientacao-a-objetos/POO/src/excecao/TesteExcecao.java
@@ -0,0 +1,27 @@
+package excecao;
+
+public class TesteExcecao {
+	public static void main(String[] args) {
+		System.out.println("Inicio main");
+		metodo1();
+		System.out.println("Fim main");
+	}
+	
+	static void metodo1(){
+		System.out.println("Inicio metodo 1");
+		metodo2();
+		System.out.println("Fim metodo 1");
+	}
+
+	static void metodo2() {
+		System.out.println("Inicio metodo 2");
+		int[] array = new int[10];
+		for(int i = 0; i <= 15; i++) {
+			array[i] = i;
+			System.out.println(i);
+		}
+		System.out.println("Fim metodo 2");
+		
+	}
+
+}
