da51ebe145839c2536673535aecd955aeda016b6
Paulo K3yboard <paulo.desenvolvedorjava@gmail.com>
2016-09-27 22:17:31 -0300

    Java 2.6.8 - Tratando uma excecao

--- java-II-orientacao-a-objetos/POO/src/contas/TestaDeposita.java
+++ java-II-orientacao-a-objetos/POO/src/contas/TestaDeposita.java
@@ -1,10 +1,13 @@
 package contas;
 
 public class TestaDeposita {
-	
+
 	public static void main(String[] args) {
 		Conta cc = new ContaCorrente();
-		cc.deposita(-100);
+		try {
+			cc.deposita(-100);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Não foi possivel depositar o valor informado " + e);
+		}
 	}
-
 }
