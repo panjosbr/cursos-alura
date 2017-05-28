780e8edc02f8d0163daa655a362708bca0a34da0
Paulo Pereira dos Anjos <paulopereiradosanjos@outlook.com>
2017-05-28 17:38:00 -0300

    Adicionando loop e atualizando lógica para aplicar o calculo do imc para todos os pacientes e criando classe quando houver algum erro nos campos

--- javascript-introducao/js/principal.js
+++ javascript-introducao/js/principal.js
@@ -1,34 +1,40 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for(i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+	console.log(paciente);
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+	var tdImc = paciente.querySelector(".info-imc");
 
-var alturaEhValida = true;
-var pesoEhValido = true;
+	var alturaEhValida = true;
+	var pesoEhValido = true;
 
-if(peso <= 0 || peso > 1000) {
-	console.log("Peso inválido");
-	tdImc.textContent = "Peso inválido";
-	pesoEhValido = false;
+	if(peso <= 0 || peso > 1000) {
+		console.log("Peso inválido");
+		tdImc.textContent = "Peso inválido";
+		pesoEhValido = false;
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(altura < 0 || altura >= 3) {
+		console.log("Altura inválida");
+		tdImc.textContent = "Altura inválida";
+		alturaEhValida = false;
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(pesoEhValido && alturaEhValida) {
+		var imc = peso / (altura * altura);
+		tdImc.textContent = imc.toFixed(2);
+	}
 }
-
-if(altura < 0 || altura > 3) {
-	console.log("Altura inválida");
-	tdImc.textContent = "Altura inválida";
-	alturaEhValida = false;
-}
-
-if(pesoEhValido && alturaEhValida) {
-	var imc = peso / (altura * altura);
-	tdImc.textContent = imc;
-}
-
-
