a305a943f3d9a744f6f0abd7fa2f5f1d62e0cccb
Paulo Pereira dos Anjos <paulopereiradosanjos@outlook.com>
2017-05-28 21:08:31 -0300

    Adicionando HTML e CSS com formulario de inclusao de pacientes e criando função para adicionar um novo paciente

--- javascript-introducao/js/principal.js
+++ javascript-introducao/js/principal.js
@@ -5,7 +5,6 @@ var pacientes = document.querySelectorAll(".paciente");
 
 for(i = 0; i < pacientes.length; i++) {
 	var paciente = pacientes[i];
-	console.log(paciente);
 	var tdPeso = paciente.querySelector(".info-peso");
 	var tdAltura = paciente.querySelector(".info-altura");
 
@@ -38,3 +37,41 @@ for(i = 0; i < pacientes.length; i++) {
 		tdImc.textContent = imc.toFixed(2);
 	}
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener('click', function(event) {
+	event.preventDefault();
+	var form = document.querySelector('#form-adiciona');
+
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.altura.value;
+
+	var pacienteTr = document.createElement("tr");
+
+	var nomeTd = document.createElement("td");
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	nomeTd.textContent = nome;
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+
+	var tabela = document.querySelector("#tabela-pacientes")
+
+	tabela.appendChild(pacienteTr);
+
+});
+
+
+
+
