2c531772690fc28d730f2eca94abc03a55b864de
Paulo Pereira dos Anjos <paulopereiradosanjos@outlook.com>
2017-05-28 22:53:18 -0300

    Aplicando boas práticas no projeto, criacao de um arquivo para os eventos do formulário e outra para os cálculos

---
+++ javascript-introducao/js/calcula-imc.js
@@ -0,0 +1,45 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
+
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var alturaEhValida = true;
+	var pesoEhValido = true;
+
+	if(peso <= 0 || peso > 1000) {
+		console.log("Peso inválido");
+		tdImc.textContent = "Peso inválido";
+		pesoEhValido = false;
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(altura < 0 || altura >= 3.00) {
+		console.log("Altura inválida");
+		tdImc.textContent = "Altura inválida";
+		alturaEhValida = false;
+
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(pesoEhValido && alturaEhValida) {
+		var imc = calculaImc(peso, altura);
+		tdImc.textContent = imc;
+	}
+}
+
+function calculaImc(peso, altura) {
+	var imc = 0;
+	imc = peso / (altura * altura);
+	return imc.toFixed(2);
+}
