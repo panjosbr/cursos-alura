1d8cd7a3556d42cbca9d659bf0b95bea02ad83a5
pauloanjos <pauloanjos>
2017-09-26 17:40:39 -0300

    [JS Avançado I] - Melhorando helper de data - tornando métodos estáticos e adicionando regex para data inválida

--- javascript-avancado-i/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i/aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,11 +1,17 @@
 class DateHelper {
-    dataParaTexto(data) {
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
     }
 
-    textoParaData(texto) {
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+    }
+
+    static textoParaData(texto) {
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Deve estar no formato aaa-mm-dd');
+
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
