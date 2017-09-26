154114d79181a251d11954d2c6be0790a5675694
pauloanjos <pauloanjos>
2017-09-26 13:26:57 -0300

    [JS Avançado I] - Adicionando método frezze na classe Negociacao e criando propriedades/metodos para a classe

--- javascript-avancado-i/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i/aluraframe/client/js/app/models/Negociacao.js
@@ -2,24 +2,25 @@ class Negociacao {
 
     constructor(data, quantidade, valor) {
 
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
-    getVolume() {
+    get volume() {
         return this._quantidade * this._valor;
     }
 
-    getData() {
-        return this._data;
+    get data() {
+        return new Date(this._data.getTime());
     }
 
-    getQuantidade() {
+    get quantidade() {
         return this._quantidade;
     }
 
-    getValor() {
+    get valor() {
         return this._valor;
     }
 }
