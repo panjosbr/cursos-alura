57e80ceed72bd41a4be0c7a08ae8d7c5b42631fd
pauloanjos <pauloanjos>
2017-09-26 12:49:43 -0300

    [JS Avançado I] - Criando classe Negociacao e encapsulando os dados

---
+++ javascript-avancado-i/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,25 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
+}
