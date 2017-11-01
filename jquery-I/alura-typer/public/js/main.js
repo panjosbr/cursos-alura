257ab0b839cdda397a949baa1b702c2fcd9bc207
pauloanjos <pauloanjos>
2017-11-01 19:27:54 -0200

    [jQuery I] - Adicionando funções para realizar o game over/restart do jogo

--- jquery-I/alura-typer/public/js/main.js
+++ jquery-I/alura-typer/public/js/main.js
@@ -1,8 +1,53 @@
-var frase = $('.frase').text();
-var numPalavras = frase.split(' ').length;
+var campo = $('.campo-digitacao');
+var tempoInicial = $('#tempo-digitacao').text();
 
-var tamanhoFrase = $('#tamanho-frase');
-tamanhoFrase.text(numPalavras);
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $('#botao-reiniciar').click(reiniciaJogo);
+});
 
-console.log(frase);
-console.log(numPalavras);
+function atualizaTamanhoFrase() {
+    var frase = $('.frase').text();
+    var numPalavras = frase.split(' ').length;
+    var tamanhoFrase = $('#tamanho-frase');
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on('input', function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $('#contador-palavras').text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $('#contador-caracteres').text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $('#tempo-digitacao').text();
+    campo.one('focus', function() {
+        $('#botao-reiniciar').attr('disabled', true);
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $('#tempo-digitacao').text(tempoRestante);
+            if(tempoRestante < 1) {
+                campo.attr('disabled', true);
+                clearInterval(cronometroID);
+                $('#botao-reiniciar').attr('disabled', false);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr('disabled', false);
+    campo.val('');
+    $('#contador-palavras').text('0');
+    $('#contador-caracteres').text('0');
+    $('#tempo-digitacao').text(tempoInicial);
+    inicializaCronometro();
+}
