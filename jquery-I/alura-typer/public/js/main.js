8f0a080ec2018f7475a54a2c0e8bee903b86fa1a
Paulo Pereira dos Anjos <paulopereiradosanjos@outlook.com>
2017-11-10 00:23:34 -0200

    [jQuery I] - Adicionando icone e acao para remover uma linha do placar/tabela

--- jquery-I/alura-typer/public/js/main.js
+++ jquery-I/alura-typer/public/js/main.js
@@ -77,6 +77,7 @@ function inserePlacar() {
     var numPalavras = $('#contador-palavras').text();
 
     var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
     corpoTabela.append(linha);
 }
 
@@ -90,8 +91,22 @@ function novaLinha(usuario, palavras) {
     var linha = $('<tr>');
     var colunaUsuario = $('<td>').text(usuario);
     var colunaPalavras = $('<td>').text(palavras);
+    var colunaRemover = $('<td>');
+
+    var link = $('<a>').attr('href', '#').addClass('botao-remover');
+    var icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
+
+    link.append(icone);
+    colunaRemover.append(link);
+
     linha.append(colunaUsuario);
     linha.append(colunaPalavras);
+    linha.append(colunaRemover);
 
     return linha;
+}
+
+function removeLinha(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
 }
