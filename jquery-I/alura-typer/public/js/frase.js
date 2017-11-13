81166923f4650ef7db2394734b560575e82acc07
pauloanjos <pauloanjos>
2017-11-13 16:27:22 -0200

    [jQuery II] - Adicionando função para atualizar a frase e o tempo de digitação

---
+++ jquery-I/alura-typer/public/js/frase.js
@@ -0,0 +1,14 @@
+$("#botao-frase").click(fraseAleatoria);
+
+function fraseAleatoria() {
+    $.get('http://localhost:3000/frases', trocarFraseAleatoria);
+}
+
+function trocarFraseAleatoria(data) {
+    var frase = $('.frase');
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
+
+    frase.text(data[numeroAleatorio].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
--- jquery-I/alura-typer/public/js/main.js
+++ jquery-I/alura-typer/public/js/main.js
@@ -29,9 +29,14 @@ function inicializaContadores() {
     });
 }
 
+function atualizaTempoInicial(tempo) {
+    tempoInicial = tempo;
+    $('#tempo-digitacao').text(tempo);
+}
+
 function inicializaMarcadores() {
-    var frase = $(".frase").text();
     campo.on("input", function() {
+        var frase = $(".frase").text();
         var digitado = campo.val();
         var comparavel = frase.substr(0, digitado.length);
 
@@ -46,8 +51,8 @@ function inicializaMarcadores() {
 }
 
 function inicializaCronometro() {
-    var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function() {
+        var tempoRestante = $("#tempo-digitacao").text();
         var cronometroID = setInterval(function() {
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
