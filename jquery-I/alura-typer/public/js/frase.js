797081ecc9b85af31f34114f6d3f44032cfd2448
pauloanjos <pauloanjos>
2017-12-06 15:54:09 -0200

    jQuery II - Adicionando botão para buscar por frase com parâmetros

--- jquery-I/alura-typer/public/js/frase.js
+++ jquery-I/alura-typer/public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").toggle();
@@ -22,4 +23,31 @@ function trocarFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+    $('#spinner').toggle();
+
+    var fraseId = $('#frase-id').val();
+    var dados = { id: fraseId };
+
+    $.get('http://localhost:3000/frases', dados, trocaFrase)
+    .fail(function() {
+        $('#erro').toggle();
+        setTimeout(function() {
+            $('#erro').toggle();
+        }, 2000);
+    })
+    .always(function() {
+        $('#spinner').toggle();
+    });
+}
+
+function trocaFrase(data) {
+    console.log(data);
+
+    var frase = $('.frase');
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
