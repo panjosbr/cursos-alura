c613e2395dd3ff8d2f048619f6211ba18dad5785
pauloanjos <pauloanjos>
2018-01-03 17:23:08 -0200

    Javascript avançado I - Criando o template da View de negociacoes

--- javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,34 +1,37 @@
 class NegociacaoController {
-    
+
     constructor() {
-        
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
-    
+
     adiciona(event) {
-        
+
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._limpaFormulario();   
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();
     }
-    
+
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value);    
+            this._inputValor.value);
     }
-    
+
     _limpaFormulario() {
-     
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();   
+        this._inputData.focus();
     }
 }
