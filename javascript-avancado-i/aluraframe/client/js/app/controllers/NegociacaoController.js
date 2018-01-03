09fcc7cc18430de8a36640834f83806164c58093
pauloanjos <pauloanjos>
2018-01-03 18:43:41 -0200

    JS Avancado I - Criando model e view para mensagens do projeto

--- javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,15 +7,23 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
 
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicioanda com sucesso';
+
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
