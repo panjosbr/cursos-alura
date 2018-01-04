36609897cc3d6966a3c5aaabdb37f32301ae9d5d
pauloanjos <pauloanjos>
2018-01-04 14:21:36 -0200

    JS Avancado I - adicionando metodo template na View com uma exception

--- javascript-avancado-i/aluraframe/client/js/app/views/View.js
+++ javascript-avancado-i/aluraframe/client/js/app/views/View.js
@@ -3,7 +3,12 @@ class View {
         this._elemento = elemento;
     }
 
+    template() {
+
+        throw new Error('O método template deve ser implementado');
+    }
+
     update(model) {
-        this._elemento.innerHTML = this._template(model);
+        this._elemento.innerHTML = this.template(model);
     }
 }
