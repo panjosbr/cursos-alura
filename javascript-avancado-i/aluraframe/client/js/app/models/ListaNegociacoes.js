3b460aa0ee303e5aaf6e974f73a3ba6af118ebfd
pauloanjos <pauloanjos>
2018-01-08 18:23:44 -0200

    JS Avancado II - Criando get para o volumeTotal e movendo comportamento para o model e atualizando a view

--- javascript-avancado-i/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -18,4 +18,8 @@ class ListaNegociacoes {
     esvazia() {
         this._negociacoes = [];
     }
+
+    get volumeTotal() {
+        return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+    }
 }
