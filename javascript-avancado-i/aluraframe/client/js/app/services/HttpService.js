13213aafff59a928a8f033d641c9d05d617120d4
pauloanjos <pauloanjos>
2018-01-12 12:04:14 -0200

    JS Avancado II - Implementando método post no HttpService e aplicando no post.html

--- javascript-avancado-i/aluraframe/client/js/app/services/HttpService.js
+++ javascript-avancado-i/aluraframe/client/js/app/services/HttpService.js
@@ -25,4 +25,31 @@ class HttpService {
             xhr.send();
         });
     }
+
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = () => {
+
+                if(xhr.readyState == 4) {
+
+                    if(xhr.status == 200) {
+
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+
+            xhr.send(JSON.stringify(dado));
+        });
+    }
+
+
 }
