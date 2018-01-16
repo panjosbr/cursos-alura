dd906e1983f49c6aef58b149864566482252c98b
pauloanjos <pauloanjos>
2018-01-16 18:22:44 -0200

    JS Avancado III - Modificando metodo post para usar o fetch também ao inves de Promise

--- javascript-avancado-i/aluraframe/client/js/app/services/HttpService.js
+++ javascript-avancado-i/aluraframe/client/js/app/services/HttpService.js
@@ -14,28 +14,11 @@ class HttpService {
 
     post(url, dado) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.onreadystatechange = () => {
-
-                if(xhr.readyState == 4) {
-
-                    if(xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send(JSON.stringify(dado));
-        });
+        return fetch(url, {
+            headers: {'Content-type': 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handlerErrors(res));
     }
-
-
 }
