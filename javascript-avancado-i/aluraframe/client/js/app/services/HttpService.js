0f29a3c7b02719acec222ef8470ee7e226ac445b
pauloanjos <pauloanjos>
2018-01-16 18:17:57 -0200

    JS Avancado III - Mudando o metodo get para usar o fetch ao inves de Promise

--- javascript-avancado-i/aluraframe/client/js/app/services/HttpService.js
+++ javascript-avancado-i/aluraframe/client/js/app/services/HttpService.js
@@ -1,29 +1,15 @@
 class HttpService {
 
+    _handlerErrors(res) {
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url);
-
-            xhr.onreadystatechange = () => {
-
-                if(xhr.readyState == 4) {
-
-                    if(xhr.status === 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send();
-        });
+        return fetch(url)
+            .then(res => this._handlerErrors(res))
+            .then(res => res.json());
     }
 
     post(url, dado) {
