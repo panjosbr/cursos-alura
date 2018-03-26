cc75dc81c0ce6f270f038f0365e9b6b067caad55
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-03-25 22:40:59 -0300

    TS 1 - Usando jQuery com typescript e removendo comentarios na versao compilada

--- typescript/alurabank/app/ts/views/View.ts
+++ typescript/alurabank/app/ts/views/View.ts
@@ -1,13 +1,13 @@
 abstract class View<T> {
 
-    private _elemento: Element;
+    private _elemento: JQuery;
 
     constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     abstract template(model: T): string;
