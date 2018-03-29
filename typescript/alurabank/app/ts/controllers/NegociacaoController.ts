762831593fd8755070a2cf22ad1207bc983c06cb
Paulo Pereira dos Anjos <paulopereiradosanjos@outlook.com>
2018-03-29 18:29:07 -0300

    TS 2 - Importando negociações usando fetch API

--- typescript/alurabank/app/ts/controllers/NegociacaoController.ts
+++ typescript/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -50,7 +50,26 @@ export class NegociacaoController {
     }
 
     importaDados() {
-        alert('oi');
+
+        function isOk(res: Response) {
+
+            if(res.ok) {
+                 return res;
+            } else {
+                throw new Error(res.statusText);
+            }
+        }
+
+        fetch('http://localhost:8080/dados')
+            .then(res => isOk(res))
+            .then(res => res.json())
+            .then((dados: any[]) => {
+                dados
+                    .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                    .forEach(negociacao => this._negociacoes.adiciona(negociacao))
+                this._negociacoesView.update(this._negociacoes);
+            })
+            .catch(err => console.log(err.message));
     }
 }
 
