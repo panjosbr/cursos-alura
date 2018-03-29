c3383be359fe25ad68a460a6541ae0be61c933cc
Paulo Pereira dos Anjos <paulopereiradosanjos@outlook.com>
2018-03-29 16:13:47 -0300

    TS 2 - Alterando o modificador de acesso private para readonly da classe Negociacao

--- typescript/alurabank/app/ts/models/Negociacao.ts
+++ typescript/alurabank/app/ts/models/Negociacao.ts
@@ -1,20 +1,8 @@
 export class Negociacao {
 
-    constructor( private _data: Date, private _quantidade: number, private _valor:number ) {}
-
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
+    constructor( readonly data: Date, readonly quantidade: number, readonly valor:number ) {}
 
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
